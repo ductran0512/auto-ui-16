0430154cf075ea4b3b532e10e0498b38fc8f80c9
Tran Minh Duc <minhducvt0512@gmail.com>
2026-07-07 21:45:49 +0700

    [ID-02] Remind about before & after test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,26 @@
 import { test, expect } from '@playwright/test';
 
+test.beforeAll('Before all', async () => {
+  console.log('Before all');
+});
+
+
+test.beforeEach('Before each', async () => {
+  console.log('Before each');
+});
+
+
+test.afterEach('After each', async () => {
+  console.log('After each');
+});
+
+
+test.afterAll('After all', async () => {
+  console.log('After all');
+});
+
+
+
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
