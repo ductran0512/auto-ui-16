ee31a674f90498301b6e8f72cd7cec2e280cc9ee
Tran Minh Duc <minhducvt0512@gmail.com>
2026-07-27 20:50:28 +0700

    [ID-15] Verify transfer

--- tests/web-elements/components/switch.spec.ts
+++ tests/web-elements/components/switch.spec.ts
@@ -6,16 +6,6 @@ test(`Verify switch`, async ({ page }) => {
     await expect(page.getByText(`Current Value: true`)).toBeVisible();
 });
 
-// async function selectCheckBoxByLabel(label: string, action: 'check' | 'uncheck', page: Page) {
-//     let xpath = `//label[contains(concat(' ', @class, ' '), 'ant-checkbox-wrapper') and contains(normalize-space(), '${label}')]`;
-//     let locator = page.locator(xpath);
-//     let checkbox = locator.getByRole('checkbox');
-//     let isCurrentChecked = await checkbox.isChecked();
-//     if ((isCurrentChecked === true && action === 'uncheck') || (isCurrentChecked === false && action === 'check')) {
-//         await page.locator(xpath).click();
-//     }
-// }
-
 async function selectSwitchByLabel(label: string, action: 'check' | 'uncheck', page: Page) {
     let xpath = `(//div[@role='separator' and normalize-space() = '${label}']/following::*[@role='switch'])[1]`;
     let locator = page.locator(xpath);
